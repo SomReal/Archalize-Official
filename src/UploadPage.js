18f86e400efacb82ff89201089cd9cdbc372cd8f
SomReal <sohampatelbronco2009@gmail.com>
2025-04-26 16:34:25 -0400

    First commit: Archalize website

---
+++ src/UploadPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function UploadPage() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 text-gray-900 font-sans">
+      <h1 className="text-3xl font-bold">Upload Page Coming Soon...</h1>
+    </div>
+  );
+}
+
+export default UploadPage;
